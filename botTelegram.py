facf29e585bc0b70dd3d793740e05ac0c589d849
diegociarafoni <diego.ciarafoni@gmail.com>
2021-04-03 20:39:16 +0200

    customize welcome message

--- botTelegram.py
+++ botTelegram.py
@@ -1,4 +1,5 @@
 import telebot
+from pytube import YouTube
 
 TOKEN = '1775044147:AAGsOMKSzVBL8I1kyItYenhIBEDJ8PWwxbg'
 
@@ -7,20 +8,27 @@ bot = telebot.TeleBot(TOKEN)
 
 @bot.message_handler(commands=['start'])
 def send_welcome(message):
+    welcomeMessage = "Benvenuto "+message.from_user.first_name + \
+        ", sono il primo bot creato da Diego digita /help per scoprire cosa posso fare"
     bot.reply_to(
-        message, "Benvenuto, sono il primo bot creato da Diego digita /help per scoprire cosa posso fare")
+        message, welcomeMessage)
 
 
 @bot.message_handler(commands=['help'])
 def send_welcome(message):
     bot.reply_to(
-        message, "Comando 1 /comando1 \n Comando 2 /comando2")
+        message, "Scarica i tuoi video da YouTube /youtube")
 
 
 @bot.message_handler(func=lambda message: True)
 def echo_all(message):
     print(message)
     bot.reply_to(message, message.text)
+
+
+@bot.message_handler(commands=['youtube'])
+def youTube(message):
+    print(message)
 
 
 bot.polling()
